6e4e725f3ae2d225fb9d8b163ecf954e1852afda
root <root@DESKTOP-10J9EA0.localdomain>
2021-02-21 03:45:54 +0800

    created Node class, tested frontier sorting, start of general function

--- 8puzzle.py
+++ 8puzzle.py
@@ -1,9 +1,11 @@
+
 #####print table#####
 def printTable(t):
   for i in range(len(t)):
     for j in range(len(t[i])):
       print(t[i][j], end = ' ')
     print()
+  print()
 
 #####operations#####
 def findBlank(t):
@@ -35,35 +37,63 @@ def misplacedTiles(t, g):
     for j in range(len(t[i])):
       if (t[i][j] != 0) and (t[i][j] != g[i][j]):
         h += 1
-  return h;
+  return h
 
-#####Node#####
+#####solution#####
+solution = [[1, 2, 3], [4, 5, 6,], [7, 8, 0]]
+
+#####Node Class#####
+class Node:
+  def __init__(self, state, f, g, h, parent):
+    self.state = state
+    self.f = f
+    self.g = g
+    self.h = h
+    self.parent = parent
+
+#####General Function#####
+def generalSearch(problem, solution, algChoice):
+  visitedNodes = set()
+  expandedNodes = set()
+  attemptedPuzzles = set()
+  frontier = []
+  maxQueueSize = len(frontier)
+
+  node1 = Node(problem, 5, 2, 1, None)
+  node2 = Node(problem, 2, 1, 1, node1)
   
+  frontier = [node1, node2]
+  frontier.sort(key = lambda Node: Node.f)
+  for item in frontier:
+    print(item.f)
+  print(f)
 
-#####goal#####
-g = [[1, 2, 3], [4, 5, 6,], [7, 8, 0]]
+  return solution
 
 
 #####main#####
-#input
+#input and create puzzle
+print('Create your 3x3 puzzle. Make sure it can be solved')
+print('(Use 0 as a blank, numbers 1-8 for the tiles)\n')
+
 row1 = [int(item) for item in input('Enter 3 numbers for row 1: ').split()]
 row2 = [int(item) for item in input('Enter 3 numbers for row 2: ').split()]
 row3 = [int(item) for item in input('Enter 3 numbers for row 3: ').split()]
+print()
 
-#create puzzle
-t = [row1, row2, row3] #t for table
-printTable(t)
+print('Your puzzle is:')
+problem = set()
+problem = [row1, row2, row3] 
+printTable(problem)
 
-h = misplacedTiles(t, g)
-print(h)
+print('Your choices of algorithms are:')
+print('1. Uniform Cost Search')
+print('2. A* with Misplaced Tile heuristic')
+print('3. A* with Manhattan distance heuristic\n')
 
-if t == g:
-  print("True")
-else:
-  print("False")
+algChoice = int(input('Enter your algorithm of choice (1-3): '))
 
-s = set()
-s = t
-print(s)
+test = generalSearch(problem, solution, algChoice)
+print(test)
 
 
