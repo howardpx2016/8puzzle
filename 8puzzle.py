0d473ac1f7721ad8e5c5cb735af6eac59d4e20bd
root <root@DESKTOP-10J9EA0.localdomain>
2021-02-21 09:10:30 +0800

    depth 1 and 2 work except for max queue size, still need manhattan

--- 8puzzle.py
+++ 8puzzle.py
@@ -56,13 +56,14 @@ def misplacedTiles(t, s):
         h += 1
   return h
 
+
 #####global variables#####
-solution = [[1, 2, 3], [4, 5, 6,], [7, 8, 0]]
+solution = [[1, 2, 3,], [4, 5, 6,], [7, 8, 0]]
 visitedNodes = set()
 expandedNodes = set()
 frontier = []
-maxQueueSize = 0
 duplicate = []
+maxQueueSize = 0
 
 #####Node Class#####
 class Node:
@@ -100,64 +101,70 @@ def findDuplicates(visitedNodes, duplicate):
   return False
 
 #####General Function#####
-def generalSearch(problem, solution, algChoice):
+def generalSearch(problem, solution, algChoice, maxQueueSize):
+  root = Node(problem, 0, 0, 0, None)
+
+  if (algChoice == 1):
+    root.h = 0
+  elif (algChoice == 2):
+    root.h = misplacedTiles(root.state, solution)
+  root.f = root.g + root.h
+
+  frontier.append(root)
   maxQueueSize = len(frontier)
-
-  # node1 = Node(problem, 5, 2, 1, None)
-  # node2 = Node(problem, 2, 1, 1, node1)
-  
-  # frontier = [node1, node2]
-
-
-  frontier.append(Node(problem, 0, 0, 0, None))
-  first = frontier[0]
+  print("og: ", maxQueueSize)
+  expandedNodes.add(root)
 
   while len(frontier) > 0:
+    if (len(frontier) > maxQueueSize):
+      maxQueueSize = len(frontier)
+    print("next: ", maxQueueSize)
     curr = frontier[0]
     frontier.pop(0)
-    expandedNodes.add(curr)
+
     #if found, return curr
-    if curr.state == solution:
+    if misplacedTiles(curr.state, solution) == 0:
       # print("right")
       return curr
-    else:
-      r, c = findBlank(curr.state) 
-      ###operations###
-      #up
-      duplicate = copy.deepcopy(curr.state)
-      if r > 0:
-        up(duplicate, r, c)
-        createChild(algChoice, curr, duplicate, frontier, visitedNodes, expandedNodes)
-      #down
-      duplicate = copy.deepcopy(curr.state)
-      if r < 2:
-        down(duplicate, r, c)
-        createChild(algChoice, curr, duplicate, frontier, visitedNodes, expandedNodes)
-      #left
-      duplicate = copy.deepcopy(curr.state)
-      if c > 0:
-        left(duplicate, r, c)
-        createChild(algChoice, curr, duplicate, frontier, visitedNodes, expandedNodes)
-      #right
-      duplicate = copy.deepcopy(curr.state)
-      if c < 2:
-        right(duplicate, r, c)
-        createChild(algChoice, curr, duplicate, frontier, visitedNodes, expandedNodes)
 
-      ###sort frontier based on f(n)###
-      frontier.sort(key = lambda Node: Node.f)
-      # #test
-      # for item in frontier:
-      #   printNode(item)
-      # print()
+    ####else####
+    r, c = findBlank(curr.state) 
+    #operations
+    #up
+    duplicate = copy.deepcopy(curr.state)
+    if r > 0:
+      up(duplicate, r, c)
+      createChild(algChoice, curr, duplicate, frontier, visitedNodes, expandedNodes)
+    #down
+    duplicate = copy.deepcopy(curr.state)
+    if r < 2:
+      down(duplicate, r, c)
+      createChild(algChoice, curr, duplicate, frontier, visitedNodes, expandedNodes)
+    #left
+    duplicate = copy.deepcopy(curr.state)
+    if c > 0:
+      left(duplicate, r, c)
+      createChild(algChoice, curr, duplicate, frontier, visitedNodes, expandedNodes)
+    #right
+    duplicate = copy.deepcopy(curr.state)
+    if c < 2:
+      right(duplicate, r, c)
+      createChild(algChoice, curr, duplicate, frontier, visitedNodes, expandedNodes)
 
-      if (len(frontier) > maxQueueSize):
-        maxQueueSize = len(frontier)
-      visitedNodes.add(curr)
+    ###sort frontier based on f(n)###
+    frontier.sort(key = lambda Node: Node.f)
+    # #test
+    # for item in frontier:
+    #   printNode(item)
+    # print()
 
-      return Node(duplicate, 1, 1, 1, None)
+    visitedNodes.add(curr)
 
-  return problem
+    printNode(curr)
+    print(len(visitedNodes))
+    print(len(expandedNodes))
+
+  return None
   
 
 
@@ -182,25 +189,27 @@ print('2. A* with Misplaced Tile heuristic')
 print('3. A* with Manhattan distance heuristic\n')
 
 algChoice = int(input('Enter your algorithm of choice (1-3): '))
+print()
 
 tick = time.perf_counter()
-test = generalSearch(problem, solution, algChoice)
+test = generalSearch(problem, solution, algChoice, maxQueueSize)
 tock = time.perf_counter()
 totalTime = tock - tick
 
-# #test
-# print("attempted puzzles:")
-# for item in visitedNodes:
-#   printTable(item.state)
 
-# print("max Queue Size: ", maxQueueSize)
-# print("number of puzzles assessed: ", len(visitedNodes))
-# print("number of expanded nodes", len(expandedNodes), '\n')
-# print("expanded Nodes:")
+
+if (test == None):
+  print('Solution was not Found')
+else:
+  print('Solution Found!')
+  print('Depth: ', test.g)
+  print("max Queue Size: ", maxQueueSize)
+  print("number of puzzles assessed: ", len(visitedNodes))
+  print("number of expanded nodes", len(expandedNodes), '\n')
+  printNode(test)
+
+print('Time elpased (seconds): ', totalTime)
+
+# #test
 # for item in expandedNodes:
 #   printNode(item)
-
-printTable(test.state)
-print(totalTime)
-
-
